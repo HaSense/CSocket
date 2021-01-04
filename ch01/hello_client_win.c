ffd90332074da1b674ef55384c03b1cb6bca9a9b
HaSense <virtual_sense@naver.com>
2021-01-04 15:45:48 +0900

    Update hello_client_win.c

--- ch01/hello_client_win.c
+++ ch01/hello_client_win.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <winsock2.h>
+#include <Ws2tcpip.h>
+
 void ErrorHandling(char* message);
 
 int main(int argc, char* argv[])
@@ -12,31 +14,32 @@ int main(int argc, char* argv[])
 	char message[30];
 	int strLen;
 
-	if(argc!=3)
+	if (argc != 3)
 	{
 		printf("Usage : %s <IP> <port>\n", argv[0]);
 		exit(1);
 	}
 
-	if(WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
-		ErrorHandling("WSAStartup() error!");  
-	
-	hSocket=socket(PF_INET, SOCK_STREAM, 0);
-	if(hSocket==INVALID_SOCKET)
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		ErrorHandling("WSAStartup() error!");
+
+	hSocket = socket(PF_INET, SOCK_STREAM, 0);
+	if (hSocket == INVALID_SOCKET)
 		ErrorHandling("socket() error");
-	
+
 	memset(&servAddr, 0, sizeof(servAddr));
-	servAddr.sin_family=AF_INET;
-	servAddr.sin_addr.s_addr=inet_addr(argv[1]);
-	servAddr.sin_port=htons(atoi(argv[2]));
-	
-	if(connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr))==SOCKET_ERROR)
+	servAddr.sin_family = AF_INET;
+	//servAddr.sin_addr.s_addr = inet_addr(argv[1]);	//vc의 경우 sdl을 끄면 사용할 수 있다
+	inet_pton(AF_INET, argv[1], &servAddr.sin_addr);
+	servAddr.sin_port = htons(atoi(argv[2]));
+
+	if (connect(hSocket, (SOCKADDR*)&servAddr, sizeof(servAddr)) == SOCKET_ERROR)
 		ErrorHandling("connect() error!");
- 
-	strLen=recv(hSocket, message, sizeof(message)-1, 0);
-	if(strLen==-1)
+
+	strLen = recv(hSocket, message, sizeof(message) - 1, 0);
+	if (strLen == -1)
 		ErrorHandling("read() error!");
-	printf("Message from server: %s \n", message);  
+	printf("Message from server: %s \n", message);
 
 	closesocket(hSocket);
 	WSACleanup();
